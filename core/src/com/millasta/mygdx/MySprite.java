6b51f09adce130fcc85d9dcf6c3e99ddfc432361
Millasta <valentin.maurice@etu.univ-tours.fr>
2015-11-29 13:23:42 +0100

    Update

---
+++ core/src/com/millasta/mygdx/MySprite.java
@@ -0,0 +1,89 @@
+package com.millasta.mygdx;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Custom class for the use of 2D single graphics
+ * @author Millasta
+ * This include all the possibilities of the LibGDX sprite class and the Animation one.
+ */
+public class MySprite {
+
+	Texture asset;
+	Sprite sprite;
+	
+	float posX;
+	float posY;
+	float rotation;
+	
+	MySprite() {
+		sprite = new Sprite();
+	}
+	MySprite(String assetPath) {
+		asset = new Texture(Gdx.files.internal(assetPath));
+		sprite = new Sprite(asset);
+		posX = 0;
+		posY = 0;
+		rotation = 0;
+		sprite.setPosition(posX, posY);
+		sprite.setRotation(rotation);
+	}
+	MySprite(String assetPath, float x, float y) {
+		asset = new Texture(Gdx.files.internal(assetPath));
+		sprite = new Sprite(asset);
+		posX = x;
+		posY = y;
+		rotation = 0;
+		update();
+	}
+	MySprite(String assetPath, float x, float y, float width, float height) {
+		asset = new Texture(Gdx.files.internal(assetPath));
+		sprite = new Sprite(asset);
+		posX = x;
+		posY = y;
+		sprite.setSize(width, height);
+		rotation = 0;
+		update();
+	}
+	
+	public void draw(SpriteBatch batch) {
+		this.sprite.draw(batch);
+	}
+	
+	public Sprite getSprite() {
+		return this.sprite;
+	}
+	
+	/**
+	 * Update following properties :
+	 * 	Position
+	 * 	Rotation
+	 */
+	public void update() {
+		this.sprite.setPosition(posX, posY);
+		this.sprite.setRotation(rotation);
+	}
+	
+	/**
+	 * Make a translation
+	 * @param x float : horizontal translation
+	 * @param y float : vertical translation
+	 */
+	public void move(float x, float y) {
+		this.posX += x;
+		this.posY += y;
+	}
+	
+	/**
+	 * Set the current position
+	 * @param x float : new x position
+	 * @param y float : new y position
+	 */
+	public void setPosition(float x, float y) {
+		this.posX = x;
+		this.posY = y;
+	}
+}

e616cbf799511aabe24c2fec6fab24a54a379e16
Millasta <valentin.maurice@etu.univ-tours.fr>
2015-11-29 12:08:13 +0100

    Adding the project files

---
+++ desktop/src/com/millasta/mygdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.millasta.mygdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.millasta.mygdx.MyGDX;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGDX(), config);
+	}
+}

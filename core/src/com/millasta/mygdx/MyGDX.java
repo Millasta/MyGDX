5247f4f39a157f268f0a6cc35c56e0b127905b7d
Millasta <valentin.maurice@etu.univ-tours.fr>
2015-11-29 12:10:24 +0100

    Update

--- core/src/com/millasta/mygdx/MyGDX.java
+++ core/src/com/millasta/mygdx/MyGDX.java
@@ -16,7 +16,7 @@ public class MyGDX extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
 		batch.begin();

e58be9c672a1c549c80e1a5e9e63a59153681487
Millasta <valentin.maurice@etu.univ-tours.fr>
2015-11-30 19:06:02 +0100

    Update

--- core/src/com/millasta/mygdx/MyGDX.java
+++ core/src/com/millasta/mygdx/MyGDX.java
@@ -8,10 +8,14 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGDX extends ApplicationAdapter {
 	SpriteBatch batch;
+	MySprite mushroom;
+	MyAnimation explosion;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
+		explosion = new MyAnimation("explosion.png", 5, 5, 0.05f, true);
+		mushroom = new MySprite("mushroom.png", Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2);
 	}
 
 	@Override
@@ -19,7 +23,12 @@ public class MyGDX extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		
+		explosion.update(Gdx.graphics.getDeltaTime());
+		mushroom.setTextureRegion(explosion.getCurrentFrame());
+		
 		batch.begin();
+			mushroom.draw(batch);
+			batch.draw(explosion.getCurrentFrame(), 100, 100);
 		batch.end();
 	}
 }

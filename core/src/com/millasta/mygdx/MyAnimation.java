691b7dcf2ba905f13f4e8e2bd61828a3bcca0251
Millasta <valentin.maurice@etu.univ-tours.fr>
2015-11-30 18:05:48 +0100

    Adding file

---
+++ core/src/com/millasta/mygdx/MyAnimation.java
@@ -0,0 +1,11 @@
+package com.millasta.mygdx;
+
+/**
+ * Custom class for the use of 2D single graphics
+ * @author Millasta
+ * This include all the possibilities of the LibGDX Animation class.
+ */
+public class MyAnimation {
+
+	
+}
